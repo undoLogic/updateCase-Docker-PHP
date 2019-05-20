c9737ce7a9657b010b8bbe44304bb92e22aa6989
sacha-lewis <sacha@undologic.com>
2019-05-20 14:56:25 -0400

    creating install script

--- install-cakePHP-2.sh
+++ install-cakePHP-2.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-#change to another name if you want
-ProjectName="NewProject"
+#public can run this to get started
+#curl http://install.updatecase.com/cakePHP2 --output install-cakePHP2.sh
+#curl https://raw.githubusercontent.com/undoLogic/updateCase-Docker-PHP/master/install-cakePHP-2.sh --output install-cakePHP-2.sh
+#This will download the file, then
+#chmod install-cakePHP-2.sh
+#./install-cakePHP-2.sh
 
+#change to another name if you want
+ProjectName="newProject"
 svn export https://github.com/undologic/updateCase-Docker-PHP/trunk/. $ProjectName/.
 curl -L -o cakephp2.zip https://github.com/cakephp/cakephp/archive/2.x.zip
 unzip cakephp2.zip
